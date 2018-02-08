faff0f92b33a6400c714d873e80c5766487095d3
muruges <muruges95@hotmail.com>
2018-02-08 18:13:32 +0800

    Added catch for situation when storage file become read-only whilst the program is running

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -23,7 +23,7 @@ import seedu.addressbook.ui.TextUi;
 public class Main {
 
     /** Version info of the program. */
-    public static final String VERSION = "AddressBook Level 2 - Version 1.0";
+    private static final String VERSION = "AddressBook Level 2 - Version 1.0";
 
     private TextUi ui;
     private StorageFile storage;
@@ -38,7 +38,7 @@ public class Main {
     }
 
     /** Runs the program until termination.  */
-    public void run(String[] launchArgs) {
+    private void run(String[] launchArgs) {
         start(launchArgs);
         runCommandLoopUntilExitCommand();
         exit();
@@ -111,6 +111,9 @@ public class Main {
             CommandResult result = command.execute();
             storage.save(addressBook);
             return result;
+        } catch (StorageOperationException e) {
+            ui.showToUser("An error has occurred: The storage file is currently read-only.");
+            return new CommandResult("");
         } catch (Exception e) {
             ui.showToUser(e.getMessage());
             throw new RuntimeException(e);

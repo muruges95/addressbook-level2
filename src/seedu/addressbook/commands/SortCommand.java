b15a2293a039ef9a381ffdb871e40c32ec28dc42
Muruges <muruges95@hotmail.com>
2018-02-11 21:44:05 +0800

    Added sort command which for now just replicates what list does

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,26 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.List;
+
+
+/**
+ * Sorts all persons by name then lists all persons in the address book to the user.
+ */
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Sorts all persons by alphabetical order of their names then displays"
+            + " all persons in the address book as a list with index numbers.\n"
+            + "Example: " + COMMAND_WORD;
+
+
+    @Override
+    public CommandResult execute() {
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+    }
+}
